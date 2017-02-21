2b841cacfdcbd7b83d487f470ec09b774ca5b8bf
Leydson Vieira <leydson.vieira@gmail.com>
2017-02-21 17:18:52 -0300

    Aplicando auto-indent

--- src/projeto.ino
+++ src/projeto.ino
@@ -23,52 +23,53 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   irrecv.enableIRIn(); // Start the receiver
+  
 }
 
 void loop() {
-   
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
+
   unsigned long duracao = pulseIn(echoPin, HIGH);
   distancia = duracao / 58;
-  
+
   Serial.print("Distancia em CM: ");
   Serial.println(distancia);
   delay(distancia*100-30);
-  
+
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX);
     irrecv.resume(); // Receive the next value
   }
-  
+
   if (results.value == 0xFFC23D) {
-        
-   if (distancia < distanciaAlarme) {
-       
-     digitalWrite(ledverdPin, LOW);    
-     digitalWrite(ledvermPin, HIGH);
-     digitalWrite(buzzPin, HIGH);
-     delay(distancia*100-50);
-     digitalWrite(ledvermPin, LOW);
-     digitalWrite(buzzPin, LOW);
-    
+
+    if (distancia < distanciaAlarme) {
+
+      digitalWrite(ledverdPin, LOW);
+      digitalWrite(ledvermPin, HIGH);
+      digitalWrite(buzzPin, HIGH);
+      delay(distancia*100-50);
+      digitalWrite(ledvermPin, LOW);
+      digitalWrite(buzzPin, LOW);
+
+    } else {
+
+      digitalWrite(ledvermPin, LOW);
+      digitalWrite(ledverdPin, HIGH);
+
+    }
+
   } else {
-    
-    digitalWrite(ledvermPin, LOW);
-    digitalWrite(ledverdPin, HIGH);
-    
+
+    desarma();
+
   }
- 
- } else {
-  
-   desarma();
-   
- }
- 
+
 }
 
 void desarma() {
